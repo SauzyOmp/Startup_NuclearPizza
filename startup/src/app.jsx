39a50c85ca7d08cc5bbfcd56392f46242c5d6f3c
SauzyOmp <124306942+SauzyOmp@users.noreply.github.com>
2025-02-13 21:51:06 -0700

    nav working?

--- startup/src/app.jsx
+++ startup/src/app.jsx
@@ -2,25 +2,33 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Map } from './map/map';
+import { Friends } from './friends/friends';
+
+
 export default function App() {
     return (
-    <div>
-        <header>
-          <h1>☢️NuclearPizza🍕</h1>
-          <nav>
-              <ul>
-                  <li><a href="map.html">Map</a></li>
-                  <li><a href="friends.html">Friends</a></li>
-              </ul>
-          </nav>
-        </header>
-  
-        <main>App components go here</main>
-  
-            <footer>
-            <p>Brandon Monson</p>
-            <a href="https://github.com/SauzyOmp/Startup_NuclearPizza" class="github-btn">GitHub</a>
-        </footer>
-      </div>
+      <BrowserRouter>
+            <div>
+                <header>
+                <h1>☢️NuclearPizza🍕</h1>
+                <nav>
+                    <ul>
+                        <li><NavLink className='nav-link' to='friends'>Friends</NavLink></li>
+                        <li><NavLink className='nav-link' to='map'>Map</NavLink></li>
+                    </ul>
+                </nav>
+                </header>
+        
+                <main>App components go here</main>
+        
+                    <footer>
+                    <p>Brandon Monson</p>
+                    <a href="https://github.com/SauzyOmp/Startup_NuclearPizza" class="github-btn">GitHub</a>
+                </footer>
+            </div>
+        </BrowserRouter>
     );
-  }
+}
